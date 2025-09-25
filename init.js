6fde4a4a06d30397582e39c1d4611ac74b661edb
Harley Jet Bagtas <hjpbagtas@bpsu.edu.ph>
2025-09-25 14:31:19 +0800

    Added title changer for pages based on dataset attribute.

--- init.js
+++ init.js
@@ -1,35 +1,48 @@
-// Declare pages and cards here to load
+// Config
 const pages = ["about"];
 const cards = ["header"];
+
+
+// Initialization
+const page = {};
+const card = {};
 async function init() {
     page["#home"] = document.getElementById("home").outerHTML;
     page["404"] = await loadTemplate("./pages/404.html");
+
     for (const c of cards) {
         card[c] = await loadTemplate(`./cards/${c}.html`);
     }
+
     window.addEventListener("hashchange", showPage);
     showPage();
 }
 
-
-// Adjusts the content of <page> based on the URL hash
+// Routing
 async function showPage() {
     const hash = location.hash || "#home";
-    if (!page[hash]) page[hash] = await loadTemplate(`./pages/${hash.replace("#", "")}.html`);
-    document.querySelector("page").outerHTML = page[hash] || page["404"];
-    initCards();
-}
+    if (!page[hash]) {
+        page[hash] = await loadTemplate(`./pages/${hash.replace("#", "")}.html`);
+    }
 
-const page = {};
-const card = {};
+    document.querySelector("page").outerHTML = page[hash] || page["404"];
+    const newpage = document.querySelector("page");
+    
+    // Parses dataset attributes like title
+    if (newpage.hasAttribute("data-title")) {
+        document.title = newpage.dataset.title;
+        newpage.removeAttribute("data-title");
+    }
+    // Will be expected to add more dataset parsing in the future
+
+    for (const element of document.getElementsByTagName("card")) {
+        element.outerHTML = card[element.id];
+    }
+}
 
 async function loadTemplate(path) {
     const res = await fetch(path + "?v=" + Date.now(), { cache: "no-store" });
     return await res.text();
 }
-function initCards() {
-    for (const element of document.getElementsByTagName("card")) {
-        element.outerHTML = card[element.id];
-    }
-}
+
 init();
