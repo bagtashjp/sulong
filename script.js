02a43163958ddb2acc1633dd8a76ac8cdb0f93b3
Harley Jet Bagtas <hjpbagtas@bpsu.edu.ph>
2025-09-25 12:47:34 +0800

    Refined modularized system. Worked with header.

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-
 async function init() {
   page = {
     home: document.getElementById("home").innerHTML,
     about: await loadTemplate("./pages/about.html")
   };
+  card = {
+    header: await loadTemplate("./cards/header.html")
+  }
   window.addEventListener("hashchange", showPage);
   showPage();
 }
 
 
+// Adjusts the content of <page> based on the URL hash
 function showPage() {
   const hash = location.hash || "#home";
-  document.getElementsByTagName("page")[0].innerHTML = page[hash.replace("#", "")] || "<h1>404 Not Found</h1>";
+  if (hash != "#home") {
+    document.getElementsByTagName("page")[0].outerHTML = page[hash.replace("#", "")] || "<h1>404 Not Found</h1>";
+  }
+  initCards();
 }
-
-// Modularizer
 
 let page = {};
-
+let card = {};
 async function loadTemplate(path) {
-  const res = await fetch(path);
-  const html = await res.text();
-  return html;
+  const res = await fetch(path + "?v=" + Date.now(), { cache: "no-store" });
+  return await res.text();
 }
-
-
-
+function initCards() {
+    for (const element of document.getElementsByTagName("card")) {
+        element.outerHTML = card[element.id];
+    }
+}
 init();
