cb43c4fc163d140b14b4f70f08f90b00cd8709ec
Harley Jet Bagtas <hjpbagtas@bpsu.edu.ph>
2025-09-29 17:39:57 +0800

    Add Test Firebase Auth.

---
+++ auth.js
@@ -0,0 +1,111 @@
+import { auth } from "/scripts/init-firebase.js";
+import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+
+let isUISignin = true;
+function switchAuthUI() {
+    const switcherBtn = document.querySelector("#ui-switch");
+    const bigButton = document.querySelector("#auth-button");
+    const bigHeading = document.querySelector("#auth-big-heading");
+    const passwordForgot = document.querySelector("#forgot-password");
+    const confirmPassField = document.querySelector("#confirm-pass-group");
+    if (isUISignin) {
+        switcherBtn.innerText = "Already have an Account? Sign-in.";
+        bigButton.innerText = "Register";
+        bigButton.classList.add("register-button")
+        bigButton.removeAttribute("onclick");
+        bigButton.setAttribute("onclick", "registerUI()");
+        bigHeading.innerText = "Register";
+        passwordForgot.style.display = "none";
+        confirmPassField.style.display = "block";
+        isUISignin = false;
+    } else {
+        switcherBtn.innerText = "Don't have an Account? Register.";
+        bigButton.innerText = "Sign In";
+        bigButton.classList.remove("register-button");
+        bigButton.removeAttribute("onclick");
+        bigButton.setAttribute("onclick", "signinUI()");
+        bigHeading.innerText = "Sign-In";
+        passwordForgot.style.display = "block";
+        confirmPassField.style.display = "none";
+        isUISignin = true;
+    }
+}
+function registerUI() {
+    const email = document.querySelector("#auth-email").value;
+    const pass = document.querySelector("#auth-password").value;
+    const confirmPass = document.querySelector("#auth-confirmpass").value;
+    if (!email) {
+        alert("Email is required!"); 
+    } else if (!isValidEmail(email)) {
+        alert("Please enter a valid email address.");
+    } else if (pass != confirmPass) {
+        alert("Passwords do not match.");
+    } else {
+        const bigButton = document.querySelector("#auth-button");
+        bigButton.disabled = true;  
+        createUserWithEmailAndPassword(auth, email, pass)
+        .then((user) => {
+            alert("Registered successfully!Please don't spam! This is just a test");
+            bigButton.enabled = false;
+            switchAuthUI();
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert("Error: " + errorMessage);
+        });
+    }
+}
+let justOnce = false;
+async function signinUI() {
+    const email = document.querySelector("#auth-email").value;
+    const pass = document.querySelector("#auth-password").value;
+    if (!email) {
+        alert("Email is required!"); 
+    } else if (!isValidEmail(email)) {
+        alert("Please enter a valid email address.");
+    } else if (!pass) {
+        alert("Password is required!");
+    } else if (pass.length < 6) {
+        alert("Password must be at least 6 characters long.")
+    } else {
+        const bigButton = document.querySelector("#auth-button");
+        bigButton.disabled = true;
+        justOnce = true;
+        await signInWithEmailAndPassword(auth, email, pass)
+        .then((user) => {
+            alert("Signed in successfully!\nPlease don't spam! This is just a test");
+            window.location.href = "index";
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert("Error: " + errorMessage);
+        });
+    }
+
+}
+
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+
+onAuthStateChanged(auth, (user) => {
+    if (justOnce) return;
+    if (user) {
+        let isConfirm = confirm("User is already signed in. You'll be redirected to home page.\nIf you wanna test again. Pressing cancel will sign out the user.");
+        if (isConfirm) {
+            window.location.href="index";
+        } else {
+            auth.signOut();
+        }
+    } else {
+        console.log("No user is signed in.");
+    }
+});
+
+window.registerUI = registerUI;
+window.signinUI = signinUI;
+window.switchAuthUI = switchAuthUI;
+
--- scripts/init-firebase.js
+++ scripts/init-firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getAnalytics, getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
