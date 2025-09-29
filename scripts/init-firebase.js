8ba940b37ed1b28cdb0945df53d7b45332bcea56
Harley Jet Bagtas <hjpbagtas@bpsu.edu.ph>
2025-09-29 15:37:13 +0800

    Added Firebase Authentication front-end.

---
+++ scripts/init-firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAnalytics, getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCxz0u7fboXQWtjHThGjPWog_63XFAdn6M",
+    authDomain: "sulong-app.firebaseapp.com",
+    projectId: "sulong-app",
+    storageBucket: "sulong-app.firebasestorage.app",
+    messagingSenderId: "968461761887",
+    appId: "1:968461761887:web:7ef17c9921e19035ef4aea",
+    measurementId: "G-Z6G6DSVCR9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
