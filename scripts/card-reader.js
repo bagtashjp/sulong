8133957908564e2249ebe1fbb6099727011b9886
Harley Jet Bagtas <hjpbagtas@bpsu.edu.ph>
2025-10-02 05:28:41 +0800

    Work on Feed page

--- scripts/card-reader.js
+++ scripts/card-reader.js
@@ -1,13 +1,14 @@
 // Config
-const cards = ["header"];
+const cards = ["header", "left_nav"];
 
 // Initialization
 const card = {};
 async function init() {
     for (const c of cards) {
         card[c] = await loadTemplate(`cards/${c}.html`);
+        console.log(card[c]);
     }
-    for (const element of document.getElementsByTagName("card")) {
+    for (const element of Array.from(document.querySelectorAll("card"))) {
         element.outerHTML = card[element.id];
     }
 }
