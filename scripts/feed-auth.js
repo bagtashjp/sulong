00a9c457019e33ddbd440054d3e13f56552e9601
Harley Jet Bagtas <hjpbagtas@bpsu.edu.ph>
2025-10-02 15:07:56 +0800

    Add dark-mode setting persistence; Change gray colors to lighter gray

--- scripts/feed-auth.js
+++ scripts/feed-auth.js
@@ -1,34 +1,48 @@
 import { auth } from "./init-firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 
+
+let justOnce = true;
 onAuthStateChanged(auth, (user) => {
     if (!user) {
         window.location.href = "signin"
     } else {
         console.log(`Signed in as ${user.uid}`)
         document.querySelector("#loading_page").style.display = "none";
+        if (justOnce) {
+            ignite();
+        }
+        justOnce = false;
     }
 });
 
 function logout() {
     auth.signOut();
 }
+let isDark = false;
 function darkmode(evt) {
     const els = document.documentElement.style;
-    if (evt.currentTarget.innerText == "Dark Mode") {
+    if (!isDark) {
          els.setProperty("--color-base-background", "#2C2C2C")
          els.setProperty("--color-base-font", "white")
          els.setProperty("--filter-dark-mode", 1);
-         
          evt.currentTarget.innerText = "Light Mode"
+         localStorage.setItem("color-scheme", "dark");
     } else {
          els.setProperty("--color-base-background", "white")
          els.setProperty("--color-base-font", "black")
          els.setProperty("--filter-dark-mode", 0);
-         
          evt.currentTarget.innerText = "Dark Mode"
+         localStorage.setItem("color-scheme", "light");
     }
 }
+
+function ignite() {
+    if (localStorage.getItem("color-scheme") == "dark") {
+        document.querySelector("#darkmoder").click();
+    }
+}
+
 
 window.darkmode = darkmode;
 window.logout = logout;
