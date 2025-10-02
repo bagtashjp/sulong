80ff586a7dce11ef816f654f77690236cc1defec
Harley Jet Bagtas <hjpbagtas@bpsu.edu.ph>
2025-10-02 14:42:33 +0800

    Add dark mode system.

--- scripts/feed-auth.js
+++ scripts/feed-auth.js
@@ -1,23 +1,34 @@
 import { auth } from "./init-firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 
-let justonce = false;
 onAuthStateChanged(auth, (user) => {
-    if (justonce) return;
     if (!user) {
-        alert("Please log-first");
         window.location.href = "signin"
     } else {
         console.log(`Signed in as ${user.uid}`)
         document.querySelector("#loading_page").style.display = "none";
-        justonce = true;
     }
 });
 
 function logout() {
     auth.signOut();
-    window.location.href = "signin"
+}
+function darkmode(evt) {
+    const els = document.documentElement.style;
+    if (evt.currentTarget.innerText == "Dark Mode") {
+         els.setProperty("--color-base-background", "#2C2C2C")
+         els.setProperty("--color-base-font", "white")
+         els.setProperty("--filter-dark-mode", 1);
+         
+         evt.currentTarget.innerText = "Light Mode"
+    } else {
+         els.setProperty("--color-base-background", "white")
+         els.setProperty("--color-base-font", "black")
+         els.setProperty("--filter-dark-mode", 0);
+         
+         evt.currentTarget.innerText = "Dark Mode"
+    }
 }
 
+window.darkmode = darkmode;
 window.logout = logout;
-
