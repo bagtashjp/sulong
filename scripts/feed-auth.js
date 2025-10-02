d060ad630938c723cc943229c789aa008178603b
Harley Jet Bagtas <hjpbagtas@bpsu.edu.ph>
2025-10-02 10:55:04 +0800

    Add logout button.

--- scripts/feed-auth.js
+++ scripts/feed-auth.js
@@ -1,14 +1,23 @@
 import { auth } from "./init-firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 
-
-
+let justonce = false;
 onAuthStateChanged(auth, (user) => {
+    if (justonce) return;
     if (!user) {
         alert("Please log-first");
         window.location.href = "signin"
     } else {
         console.log(`Signed in as ${user.uid}`)
         document.querySelector("#loading_page").style.display = "none";
+        justonce = true;
     }
 });
+
+function logout() {
+    auth.signOut();
+    window.location.href = "signin"
+}
+
+window.logout = logout;
+
