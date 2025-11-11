05f34ff3ce0008ef7577e01d318baefa10a53e43
Harley Jet Bagtas <hjpbagtas@bpsu.edu.ph>
2025-11-11 21:43:42 +0800

    Refactor search function; update query validation and adjust request body to use query instead of embedding

--- functions/api/firestore/search.js
+++ functions/api/firestore/search.js
@@ -6,19 +6,19 @@ export async function onRequest(context) {
     const url = new URL(req.url);
     const params = url.searchParams;
     const query = params.get("query");
-    if (!query) return new Response("query is required", { status: 400 });
-    const embedding = await getEmbedding(context.env.GOOGLE_AI_KEY_A,
+    if (!query || query == "") return new Response("query is required", { status: 400 });
+    /*const embedding = await getEmbedding(context.env.GOOGLE_AI_KEY_A,
         "gemini-embedding-001",
         query,
         "RETRIEVAL_QUERY"
     );
-    if (!embedding) return new Response("Failed to generate embedding", { status: 500 });
+    if (!embedding) return new Response("Failed to generate embedding", { status: 500 });*/
     const res = await fetch(context.env.VECTOR_TOOL_URL + "/vector-search", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ vector: embedding })
+        body: JSON.stringify({ query })
     })
     if (res.ok) {
         const data = await res.json();
